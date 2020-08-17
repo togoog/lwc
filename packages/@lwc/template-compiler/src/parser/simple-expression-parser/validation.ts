68b52b92341be2b87f4ea2f13abe787b182366e5
Jose David Rodriguez Velasco <jodarove@gmail.com>
2020-08-17 16:07:44 -0700

    fix: add copyright header to added files

--- packages/@lwc/template-compiler/src/parser/simple-expression-parser/validation.ts
+++ packages/@lwc/template-compiler/src/parser/simple-expression-parser/validation.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
 import { invariant, ParserDiagnostics } from '@lwc/errors';
 import { ASTExpression } from './types';
 import { IRNode, TemplateIdentifier } from '../../shared/types';
